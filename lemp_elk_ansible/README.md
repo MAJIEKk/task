ecdf75e81e4ed27a1f5ddd39a5590dc7e786e6e5
admin <admin@MacBook-Pro.local>
2024-11-04 12:27:30 +0100

    change ansible for vm

---
+++ lemp_elk_ansible/README.md
@@ -0,0 +1,6 @@
+
+
+1. edit inventory.yaml
+2. edit roles/filebeat/templates/filebeat.yml.j2 #need ubu2 ip
+
+run: "ansible-playbook site.yml"
--- run_playbooks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ansible-playbook -i hosts.ini add_user.yml
-ansible-playbook -i inventory.ini security.yml
-ansible-playbook -i inventory.ini ssh_security.yml
-ansible-playbook -i inventory.ini lemp_setup.yml
-ansible-playbook -i inventory.ini elk_setup.yml
-ansible-playbook -i inventory.ini filebeat_setup.yml
-
